a2c66a64a578c929250055255803aad257f4478a
DESKTOP-9R3EA4E\Tec <lasse.bjoern@hotmail.dk>
2017-12-08 08:52:05 +0100

    Class Opgave 08-12

---
+++ 08-12/08-12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Der er to ting i en klasse: Fields (Properties) og Methods
+
+            
+
+        }
+    }
+}
